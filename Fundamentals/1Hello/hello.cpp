b404e0582a7bd0fbb942c79e46f79c2c94de44e7
piomark1 <153437952+piomark1@users.noreply.github.com>
2026-04-15 17:59:05 +0100

    Create hello.cpp

---
+++ Fundamentals/1Hello/hello.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+  system("clear");
+  
+  printf("************************************/n
+************Hello ASM World!********/n
+************************************/n");
+
+  getchar();
+  system("clear");
+
+  return 0;
+}
