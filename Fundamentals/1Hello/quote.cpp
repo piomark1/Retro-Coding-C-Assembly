e89aee64a8d6e1f4678c2e9aa9c5f7174cc385f7
piomark1 <153437952+piomark1@users.noreply.github.com>
2026-05-17 15:59:12 +0100

    Add files via upload

---
+++ Fundamentals/1Hello/quote.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+  printf("\"I am someone\'s quote.\"\n");
+  return 0;
+}
---
+++ Fundamentals/1Hello/quote.s
@@ -0,0 +1,37 @@
+#Compilation:
+#             gcc -fno-pie -no-pie quote.s c.cpp -lstdc++ -z noexecstack
+
+    Str:  .asciz  "\"I am someone\'s quote\"\n"
+
+.section  .data
+.section  .bss
+.section  .text
+
+ProgramOutput:
+    pushq %rbx
+    xorq  %rax, %rax
+    call  printf
+    xorq  %rax, %rax
+    popq  %rbx
+    ret
+
+    .global asmMain
+asmMain:
+
+    pushq %rbp
+    movq  %rsp, %rbp
+    pushq %rax
+    pushq %rbx
+
+    lea   Str(%rip), %rdi
+    call  ProgramOutput
+
+    popq  %rbx
+    popq  %rax
+    popq  %rbp
+
+    xorq  %rdi, %rdi
+    xorq  %rax, %rax
+    movq  $60, %rax
+    syscall
+
