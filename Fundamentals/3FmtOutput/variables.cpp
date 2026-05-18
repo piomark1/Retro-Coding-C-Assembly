333dbbf0f515cd27a181a99f8b2ddd7bd05a1d73
piomark1 <153437952+piomark1@users.noreply.github.com>
2026-05-18 22:23:49 +0100

    Add files via upload

---
+++ Fundamentals/3FmtOutput/variables.cpp
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+double        percent;
+unsigned int  MyAge;
+unsigned long range;
+int           temperature;
+float         fertilise_price;
+
+int main(void)
+{
+  percent     = 0.000001;
+  MyAge       = 20;
+  range       = 100000;
+  temperature = -5;
+  fertilise_price = 12.35;
+
+  printf("My name is John, I am %u"
+          " years old, I walk to school %lu cm out away"
+          " of my home.\n"
+          "Girls from my school have body temperature %d Celsius degrees\n"
+          "so I spent on them only %f percent of my income\n"
+          "from my extra job what is fertilise trading\n"
+          "%f GBP/kg.\n", 
+          MyAge, 
+          range,
+          temperature,
+          percent,
+          fertilise_price
+          );
+
+  return 0;
+
+}
