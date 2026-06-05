9f966bd797b0effbaa8edb27a2c8dabeb582594b
piomark1 <153437952+piomark1@users.noreply.github.com>
2026-06-05 17:59:13 +0100

    Add files via upload

---
+++ Fundamentals/7IfStatement/EqualOrNot.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int x, y, z;
+
+int main(void)
+{
+    printf("Enter first number: \n");
+    scanf("%d", &x);
+    printf("Enter sectond number: \n");
+    scanf("%d", &y);
+
+    if (x != y)
+    {
+        z = x + y;
+        printf("\nAlready calculated a sum: %d\n", z);
+    }
+    else
+    {
+        z = x/y;
+        printf("\aAlready calculated a quotient: %d\n", z);
+    }
+    return 0;
+}   
