11406ea458c662774f6b2717e9dae20c8ec6fba4
piomark1 <153437952+piomark1@users.noreply.github.com>
2026-06-21 12:36:50 +0100

    Create MyFirstFunction1.s

---
+++ 2StructuredProgramming/1MyFirstFunction/MyFirstFunction1.s
@@ -0,0 +1,26 @@
+#Compilation:
+#       gcc -fno-pie -no-pie MyFirstFunction1.s MyFirstFunction1.cpp -lstdc++ -z noexecstack
+.section .text
+
+    .global MyFirstFunction
+MyFirstFunction:
+    pushq   %rbp
+    movq    %rsp, %rbp
+    subq    $16, %rsp
+
+    lea     OutStr(%rip), %rdi
+    xorq    %rax, %rax
+    call    printf
+    xorq    %rax, %rax
+
+    addq    $16, %rsp
+    popq    %rbp
+
+    ret
+
+.section const, "a"
+    OutStr: .ascii  "I am Your first function\n"
+            .ascii  "which doesn't take any arguments\n"
+            .asciz  "and return any values\n"
+.section .data
+.section .bss
