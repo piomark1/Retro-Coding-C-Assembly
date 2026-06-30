14438663d10bdbe590315011c973aa2f8894c6f0
piomark1 <153437952+piomark1@users.noreply.github.com>
2026-06-30 06:24:46 +0100

    Create AnotherNoTakeNoRetFunc.s

---
+++ 2StructuredProgramming/3RetNothing/AnotherNoTakeNoRet/AnotherNoTakeNoRetFunc.s
@@ -0,0 +1,22 @@
+    .include "InputOutput.inc"
+
+.section .text
+    
+    .global NoTakeNoRet
+NoTakeNoRet:
+    
+    movl    $40, %eax
+    addl    $10, %eax
+
+    movl    %eax, x(%rip)
+
+    lea     OutStr(%rip), %rdi
+    movl    x(%rip), %esi
+    xorq    %rax, %rax
+    call    ProgramOutput
+    
+    ret
+
+.section const, "a"
+    OutStr: .ascii  "The sum of literal constants 10 and 40 "
+            .asciz  "is %d\n"
