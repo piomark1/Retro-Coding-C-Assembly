aba9420deb12682df17ac33b1f7820735c7090d7
piomark1 <153437952+piomark1@users.noreply.github.com>
2026-07-04 14:18:43 +0100

    Add files via upload

---
+++ 2StructuredProgramming/4TakeAndReturnFunc/TakeAndRetWWarnings.cpp
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int TakeAndRet(int x, int y, int z);
+
+int a, b, c, score;
+
+int main(void)
+{
+    printf("First number:\n");
+    scanf("%d", &a);
+    printf("Second number:\n");
+    scanf("%d", &b);
+    printf("Third number:\n");
+    scanf("%d", &c);
+
+    score = TakeAndRet(a, b, c);
+
+    printf("Score: %d\n", score);
+
+    return 0;
+
+}
+int TakeAndRet(int x, int y, int z)
+{
+    if ((x*=y) > (z*=100))
+    {
+        __asm__(
+                "movl   %edi, %eax\n"
+                "leave\n"
+                "ret"
+                );
+    }
+    else
+    {
+        __asm__(
+                "movl   %esi, %eax\n"
+                "leave\n"
+                "ret"
+                );
+
+    }
+}
