98209628a7bf50b3fef9bab1d9a477274423fd31
piomark1 <153437952+piomark1@users.noreply.github.com>
2026-07-07 02:54:16 +0100

    Add files via upload

---
+++ 2StructuredProgramming/5LoopsAndArrays/1ILoveSummer/2ILoveSummerAsm/ILoveSummerLoop.s
@@ -0,0 +1,41 @@
+#Compilation:
+#       gcc -fno-pie -no-pie ILoveSummerLoop.s c.cpp -lstdc++ -z nexecstack
+.include    "InputOutput.inc"
+.section .text
+    .global asmMain
+asmMain:
+    pushq   %rbp
+    movq    %rsp, %rbp
+    subq    $16, %rsp
+
+    xorq    %rax, %rax
+Loop:
+    cmpq    $10, %rax
+    je      ProgramExit
+
+    pushq   %rax
+    lea     OutStr(%rip), %rdi
+    incl    %eax
+    movl    %eax, n(%rip)
+    movl    n(%rip), %esi
+    xorq    %rax, %rax
+    call    ProgramOutput
+    popq    %rax
+    incq    %rax
+    jmp     Loop
+
+ProgramExit:
+
+    addq    $16, %rsp
+    popq    %rbp
+
+    xorq    %rdi, %rdi
+    movq    $60, %rax
+
+    syscall
+
+.section const, "a"
+    OutStr: .asciz  "%d: I love Summer!\n"
+.section .data
+    n:  .int    0
+.section .bss
