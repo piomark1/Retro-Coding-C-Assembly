80ff74373a1cbd411912f47d2d9f2962e9ef43d3
piomark1 <153437952+piomark1@users.noreply.github.com>
2026-07-30 06:41:40 +0100

    Create PosSumAsm.cpp with basic structure
    Initial implementation of PosSumAsm.cpp with assembly placeholders and I/O functions.

---
+++ 2StructuredProgramming/5LoopsAndArrays/7InfiniteLoop/PosSumAsmCpp/PosSumAsm.cpp
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+unsigned int number;
+unsigned long sum;
+
+int main(void)
+{
+    __asm__();
+    printf();
+    scanf();
+    __asm__();
+    printf();
+    printf();
+    return 0;
+}
