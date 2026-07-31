71646aa53a020c8e1966ce203e43bd875b9e43f8
piomark1 <153437952+piomark1@users.noreply.github.com>
2026-07-31 22:18:28 +0100

    Implement infinite loop for positive number summation

--- 2StructuredProgramming/5LoopsAndArrays/7InfiniteLoop/PosSumAsmCpp/PosSumAsm.cpp
+++ 2StructuredProgramming/5LoopsAndArrays/7InfiniteLoop/PosSumAsmCpp/PosSumAsm.cpp
@@ -6,11 +6,27 @@ unsigned long sum;
 
 int main(void)
 {
-    __asm__();
-    printf();
-    scanf();
-    __asm__();
-    printf();
-    printf();
+    __asm__("InfLoop:\n");
+    
+    printf("Enter positive number from the range "
+            "2 - 65535, enter 1 for finish\n");
+    
+    scanf("%u", &number);
+    
+    __asm__("xorq   %rax, %rax\n"
+            "movl number(%rip), %eax\n "
+            "cmpl   $1, %eax\n"
+            "je     Exit\n"
+            "xorq   %rbx, %rbx\n"
+            "movl   sum(%rip), %ebx\n"
+            "addl   %eax, %ebx\n"
+            "movl   %ebx, sum(%rip)\n"
+            "jmp    InfLoop\n"
+            "Exit:\n"
+            "   nop\n");
+    
+    printf("The sum of all entered numbers "
+            "is: %lu\n", sum);
+
     return 0;
 }
