4e762ae175645aac3e1b5d789d879b7648b8799e
piomark1 <153437952+piomark1@users.noreply.github.com>
2026-08-17 06:26:59 +0100

    Update assembly code to use LSptr for string

--- 2StructuredProgramming/6Exercises/Exercise2/ILoveCAsm/exercise2asm.cpp
+++ 2StructuredProgramming/6Exercises/Exercise2/ILoveCAsm/exercise2asm.cpp
@@ -4,6 +4,7 @@
 void ILoveC(void);
 
 char LoveString[] = "I love C!!!\n\000";
+char *LSptr = LoveString;
 
 int main(void)
 {
@@ -15,7 +16,7 @@ void ILoveC(void)
 {
      __asm__("pushq  %rbx\n"
             "xorq   %rax, %rax\n"
-            "lea    LoveString(%rip), %rdi\n"
+            "movq   LSptr(%rip), %rdi\n"  //"lea    LoveString(%rip), %rdi\n"
             "call   printf\n"
             "xorq   %rax, %rax\n"
             "popq   %rbx\n"); 
