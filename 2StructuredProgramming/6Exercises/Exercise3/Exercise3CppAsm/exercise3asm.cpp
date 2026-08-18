a174faaad0dd9b5ab1cab02ca25d6a6bfc9e9736
piomark1 <153437952+piomark1@users.noreply.github.com>
2026-08-18 21:46:13 +0100

    Add initial structure for exercise3asm.cpp
    Initial implementation of exercise3asm.cpp with function prototypes and an empty main function.

---
+++ 2StructuredProgramming/6Exercises/Exercise3/Exercise3CppAsm/exercise3asm.cpp
@@ -0,0 +1,14 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+int idx; array[];
+void SetArray(int arr[]; int val);
+
+int main(void)
+{
+  return 0;
+}
+void SetArray(int arr[]; int val)
+{
+  __asm__();
+}
