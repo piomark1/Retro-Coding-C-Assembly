7f8a59cf4b949152b7e90f4fe68479942a3f5621
piomark1 <153437952+piomark1@users.noreply.github.com>
2026-08-18 21:19:29 +0100

    Fix variable names.

--- 2StructuredProgramming/6Exercises/Exercise3/Exercise3Cpp/exercise3.cpp
+++ 2StructuredProgramming/6Exercises/Exercise3/Exercise3Cpp/exercise3.cpp
@@ -1,17 +1,17 @@
 /*
 Write a program that will assign vlue 1 to each element
-of any table.
+of any array.
 */
 #include <stdlib.h>
 #include <stdio.h>
 
-int index, table[10];
+int index, arry[10];
 
 int main(void)
 {
   for(index=0; index<10; ++index)
     {
-      table[index] = 1;
+      array[index] = 1;
     }
   return 0;
 }
